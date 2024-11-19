93d879bf45ecb570c3deb85cc30c9c12a3e514ea
Viral Vala <viral.vala@pubmatic.com>
2024-11-19 21:00:24 +0530

    UOE-11453: Removing heading and trailing whitespaces from cdata

--- etree_ow.go
+++ etree_ow.go
@@ -7,7 +7,7 @@ import (
 func (e *Element) TrimmedText() string {
 	var text string
 	for _, child := range e.Child {
-		if cd, ok := child.(*CharData); ok {
+		if cd, ok := child.(*CharData); ok && !cd.whitespace {
 			text = text + cd.Data
 		}
 	}
